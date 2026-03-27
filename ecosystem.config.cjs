2ebee987fe1c750b52da585f28cdf4bd416f66e1
mrjianggg <972577493@qq.com>
2026-03-27 11:52:17 +0800

    ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,18 +2,9 @@ module.exports = {
   apps: [
     {
       name: 'gonka-gateway',
+      port: '3001',
       script: './.output/server/index.mjs',
-      instances: 'max',        // 或 '1'，根据服务器 CPU 决定
-      exec_mode: 'cluster',    // 集群模式，提升性能
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3001,
-        HOST: '0.0.0.0'
-      },
-      // 可选：自动重启、日志等
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G'
+      max_memory_restart: '300M'
     }
   ]
-};
+}
